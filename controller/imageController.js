f2bd78b2e53cb8342124267c41e7f51a45945f8a
Leesungsup <tmdtjq0116@naver.com>
2022-12-13 01:17:56 +0900

    Use h5 model

--- controller/imageController.js
+++ controller/imageController.js
@@ -14,15 +14,7 @@ var uploadImage1 = (req,res)=>{
         var src = fs.createReadStream(tmp_path);
         var dest = fs.createWriteStream(target_path);
         src.pipe(dest);
-        var options = {
-            mode: "json", // 파이썬 응답을 어떤 형식으로 받을 것인가
-            pythonPath: "",
-            pythonOptions: ["-u"],
-            scriptPath: "",
-            args: [req.files[0].originalname],
-            // args: ['카레유', '20',req.files[0].originalname], // 파이썬 코드로 전달할 arguments
-        };
-        console.log(options); 
+        
         // predict.py 파일을 실행 시킴
         // PythonShell.run("model.py", options, function (err, results) {
         //     if (err) throw err;
@@ -43,26 +35,90 @@ var uploadImage1 = (req,res)=>{
         //             console.log("success save !!");
         //     });
         // });
-        PythonShell.run("model1.py", options, function (err, results) {
-            if (err) throw err;
-            // 전달 받은 결과값을 찍어봄
-            console.log("dga"); 
-            // res.send(results);
-            console.log(typeof(results));
-            console.log(Object.keys(results))
-            console.log(results[0].dd);
-            // console.log(results[1]);
-            //let name = req.files[0].originalname;
-            let str= req.files[0].originalname.replace('/','');
-            str=req.files[0].originalname.replace('.','');
-            database.ref("approved_users/").child(str).update({name : results[0].dd.toString()}, function(error) {
-                if(error)
-                    console.error(error)
-                else
-                    console.log("success save !!");
-            });
+        
+
+
+
+
+        // PythonShell.run("model1.py", options, function (err, results) {
+        //     if (err) throw err;
+        //     // 전달 받은 결과값을 찍어봄
+        //     console.log("dga"); 
+        //     // res.send(results);
+        //     console.log(typeof(results));
+        //     console.log(Object.keys(results))
+        //     console.log(results[0].dd);
+        //     // console.log(results[1]);
+        //     //let name = req.files[0].originalname;
+        //     let str= req.files[0].originalname.replace('/','');
+        //     str=req.files[0].originalname.replace('.','');
+        //     database.ref("approved_users/").child(str).update({name : results[0].dd.toString()}, function(error) {
+        //         if(error)
+        //             console.error(error)
+        //         else
+        //             console.log("success save !!");
+        //     });
+        // });
+        var exec = require('child_process').exec;
+        exec('py -3.10 model1.py --input_images '+req.files[0].originalname, function(error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+            else{
+                console.log('pythonshell');
+                var options = {
+                    mode: "json", // 파이썬 응답을 어떤 형식으로 받을 것인가
+                    pythonPath: "",
+                    pythonOptions: ["-u"],
+                    scriptPath: "",
+                    args: ["--input_images", "./uploads/"+req.files[0].originalname,"--depth_model_architecture", "./Food_volume_estimation/depth_architecture.json","--depth_model_weights","./Food_volume_estimation/depth_weights.h5","--segmentation_weights", "./Food_volume_estimation/segmentation_weights.h5","--food",stdout.replace(/\n|\r|\s*/g, "")]
+                    // args: [req.files[0].originalname],
+                    // args: ['카레유', '20',req.files[0].originalname], // 파이썬 코드로 전달할 arguments
+                };
+                console.log(options); 
+                PythonShell.run("./Food_volume_estimation/volume_estimator.py", options, function (err, results) {
+                    if (err) throw err;
+                    // 전달 받은 결과값을 찍어봄
+                    console.log("dga"); 
+                    // res.send(results);
+                    console.log(typeof(results));
+                    console.log(Object.keys(results))
+                    console.log(results[0].dd);
+                    // console.log(results[1]);
+                    //let name = req.files[0].originalname;
+                    let str= req.files[0].originalname.replace('/','');
+                    str=req.files[0].originalname.replace('.','');
+                    database.ref("approved_users/").child(str).update({name : results[0].dd.toString()}, function(error) {
+                        if(error)
+                            console.error(error)
+                        else
+                            console.log("success save !!");
+                    });
+                });
+            }
         });
-        const { spawn } = require('child_process');
+
+        // PythonShell.run("./Food_volume_estimation/volume_estimator.py", options, function (err, results) {
+        //     if (err) throw err;
+        //     // 전달 받은 결과값을 찍어봄
+        //     console.log("dga"); 
+        //     // res.send(results);
+        //     console.log(typeof(results));
+        //     console.log(Object.keys(results))
+        //     console.log(results[0].dd);
+        //     // console.log(results[1]);
+        //     //let name = req.files[0].originalname;
+        //     let str= req.files[0].originalname.replace('/','');
+        //     str=req.files[0].originalname.replace('.','');
+        //     database.ref("approved_users/").child(str).update({name : results[0].dd.toString()}, function(error) {
+        //         if(error)
+        //             console.error(error)
+        //         else
+        //             console.log("success save !!");
+        //     });
+        // });
+        
         //const pyProg = spawn('python', ['./../pypy.py']);
         // const result_02 = spawn('python', ['model.py',req.files[0].originalname]);
         // const result_02 = spawn('python', ['function_argv.py', '카레유', '20',req.files[0].originalname]);
--- model1.py
+++ model1.py
@@ -1,4 +1,6 @@
 import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import keras
 from matplotlib import pyplot as plt
 import matplotlib.pyplot as plt
@@ -9,6 +11,7 @@ import json
 import numpy as np
 # {'과메기': 0, '양념치킨': 1, '젓갈': 2, '콩자반': 3, '편육': 4, '피자': 5, '후라이드치킨': 6}
 cal={'0':178,'1':323,'2':100,'3':317,'4':156,'5':269,'6':293}
+cal1={'0':178,'1':323,'2':100,'3':317,'4':156,'5':'pizza','6':'chicken'}
 def load_image(img_path, show=False):
 
     img = image.load_img(img_path, target_size=(299, 299))
@@ -27,8 +30,8 @@ def load_image(img_path, show=False):
 if __name__ == '__main__':
     model = load_model('./'+'0206_InceptionV3 model.h5')
     # sample_img_path = './drive/MyDrive/food/test/콩자반/Img_025_0088.jpg'
-    sample_img_path = './uploads/1_13.jpg'
-    # sample_img_path = './uploads/'+os.sys.argv[1]
+    # sample_img_path = './uploads/1_13.jpg'
+    sample_img_path = './uploads/'+os.sys.argv[2]
     img = image.load_img(sample_img_path, target_size = (299, 299))
     
     # print(sample_img_path)
@@ -36,6 +39,7 @@ if __name__ == '__main__':
     # print(model.predict(new_image))
     classes = np.argmax(model.predict(new_image,verbose=0), axis = 1)
     # print(cal[str(classes[0])])
-    json_string={"dd":str(cal[str(classes[0])])}
-    json_object = json.dumps(json_string)
-    print(json_object)
+    # json_string={"dd":str(cal[str(classes[0])])}
+    # json_object = json.dumps(json_string)
+    # print(json_object)
+    print(cal1[str(classes[0])])
